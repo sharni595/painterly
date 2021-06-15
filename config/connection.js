89b4178395402d6009ad9f57390e3cd6dff86c3b
sharni595 <72106865+sharni595@users.noreply.github.com>
2021-06-14 20:36:47 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('todo_app', 'root', 'kool123kat', {
+  : new Sequelize('todo_app', 'root', '', {
     host: 'localhost',
     port: 3306,
     dialect: 'mysql',
