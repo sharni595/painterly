955fd658d3604add334e9bad9fa3e0516ef3e94b
kotaewing <kota.ewing@gmail.com>
2021-06-20 17:53:41 -0600

    started cloudinary routes before merging

--- controllers/api/cloudinary-routes.js
+++ controllers/api/cloudinary-routes.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
 const cloudinary = require('cloudinary').v2;
 const fileupload = require('express-fileupload');
+
+cloudinary.config({
+    cloud_name: '',
+    api_key: '',
+    api_secret: ''
+})
