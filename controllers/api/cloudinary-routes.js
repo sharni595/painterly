ba00d4b3db550eb35a67f3b2104a442cce8e1154
kotaewing <kota.ewing@gmail.com>
2021-06-21 19:59:26 -0600

    file upload route works as expected

--- controllers/api/cloudinary-routes.js
+++ controllers/api/cloudinary-routes.js
@@ -3,15 +3,19 @@ const cloudinary = require('cloudinary').v2;
 const fileupload = require('express-fileupload');
 // require('dotenv').config();
 
-
+cloudinary.config({
+    cloud_name: process.env.CC_NAME,
+    api_key: process.env.CC_KEY,
+    api_secret: process.env.CC_SECRET
+  });
+  
 
 router.use(fileupload({
     useTempFiles: true
 }));
 
-router.post('/upload', (req, res) => {
+router.post('/', (req, res) => {
     const file = req.files.photo;
-    console.log(file);
     cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
         if (err) {
             console.log('Cloudinary error: ' + err);
