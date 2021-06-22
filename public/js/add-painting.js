4b57fcf38947fee756dca4c4aaa1c2e58e081ce2
kotaewing <kota.ewing@gmail.com>
2021-06-21 19:16:28 -0600

    added functions for drawing

--- public/js/add-painting.js
+++ public/js/add-painting.js
@@ -7,6 +7,41 @@ const paths = [];
 const currentPath = [];
 var canvas;
 
+const setup = () => {
+    canvas = createCanvas(window.innerWidth, window.innerHeight);
+    background(255);
+};
+
+const draw = () => {
+    noFill();
+
+    if(mouseIsPressed) {
+        const point = {
+            x: mouseX,
+            y: mouseY,
+            color: colorInput.value,
+            weight: weight.value
+        };
+
+        currentPath.push(point);
+    }
+
+    paths.forEach(path => {
+        beginShape();
+        path.forEach(point => {
+            stroke(point.color);
+            strokeWeight(point.weight);
+            vertex(point.x, point.y);
+        });
+        endShape();
+    });
+}
+
+const mousePressed = () => {
+    currentPath = [];
+    paths.push(currentPath);
+}
+
 clear.addEventListener('click', () => {
     paths.splice(0);
     background(255);
