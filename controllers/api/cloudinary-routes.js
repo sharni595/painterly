3604054e245fb62dbdf1751816d0ed0bfc9a8b35
kotaewing <kota.ewing@gmail.com>
2021-06-21 19:24:35 -0600

    added base code for cloudinary post

--- controllers/api/cloudinary-routes.js
+++ controllers/api/cloudinary-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const cloudinary = require('cloudinary').v2;
 const fileupload = require('express-fileupload');
+require('dotenv')
 
 cloudinary.config({
     cloud_name: '',
@@ -8,3 +9,20 @@ cloudinary.config({
     api_secret: ''
 });
 
+router.use(fileupload({
+    useTempFiles: true
+}));
+
+router.post('/upload', (req, res) => {
+    const file = req.files.photo;
+    console.log(file);
+    cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
+        if (err) {
+            console.log('Cloudinary error: ' + err);
+        }
+        res.json({
+            success: true,
+            result
+        })
+    })
+})
