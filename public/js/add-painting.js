215d85db168ad84ba15e5a931f8976f174b68c93
kotaewing <kota.ewing@gmail.com>
2021-06-21 19:00:51 -0600

    added code to add painting.js for buttons and dom elements

--- public/js/add-painting.js
+++ public/js/add-painting.js
@@ -1,1 +1,17 @@
 //i think this is the js file that will have the fetch post requests that link to controllers/painting-routes.js
+const colorInput = document.getElementById('color-selection');
+const weight = document.getElementById('line-weight');
+const clear = document.getElementById('clear-canvas');
+const save = document.getElementById('save-canvas');
+const paths = [];
+const currentPath = [];
+var canvas;
+
+clear.addEventListener('click', () => {
+    paths.splice(0);
+    background(255);
+});
+
+save.addEventListener('click', () => {
+    saveCanvas('painterly-canvas', 'jpg');
+});
